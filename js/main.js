01682d7f19dd03c7dd33439b8081bc45ff3b80a2
YanaAduevskaya <kambel91@mail.ru>
2023-07-28 23:18:27 +0300

    add adaptive and menu-burger

--- js/main.js
+++ js/main.js
@@ -2,3 +2,7 @@ document.addEventListener('DOMContentLoaded', function() {
     var elems = document.querySelectorAll('.materialboxed');
     var instances = M.Materialbox.init(elems);
   });
+  document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.sidenav');
+    var instances = M.Sidenav.init(elems);
+  });
